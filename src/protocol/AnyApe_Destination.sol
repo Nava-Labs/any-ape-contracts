904027c70682242a806cd4941f77107df52e4a2f
rwin <erwinphanglius10@gmail.com>
2023-10-15 03:16:51 +0700

    add source and destination marketplace contracts

---
+++ src/protocol/AnyApe_Destination.sol
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {Withdraw} from "../utils/Withdraw.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+error Unauthorized();
+
+contract AnyApe_Destination is CCIPReceiver, Withdraw {
+
+    uint64 constant SOURCE_CHAIN_SELECTOR = 12532609583862916517; // mumbai
+    uint64 constant DEST_CHAIN_SELECTOR = 14767482510784806043; // fuji
+
+    address public receiver;
+
+    address immutable i_router;
+    address immutable i_link;
+    address immutable public APE;
+
+    enum BuyType {
+        SourceChain,
+        CrossChain
+    }
+
+    struct ListingDetails {
+        address listedBy;
+        uint256 price;
+    }
+    // data contains should be eq to source chain
+    mapping(address => mapping(uint256 => ListingDetails)) private _listingDetails; // tokenAddress => tokenId => ListingDetails
+
+    struct CrossChainBuy {
+        address buyer;
+    }
+
+    event Buy(
+        uint256 indexed timestamp, 
+        BuyType indexed buyType,
+        address indexed userAddress, 
+        address tokenAddress, 
+        uint256 tokenId,
+        uint256 price
+    );
+
+    event MessageSent(bytes32 messageId, bytes data);
+
+    event MessageReceived(bytes32 messageId, bytes data);
+
+    constructor(address router, address link, address ape) CCIPReceiver(router) {
+        i_router = router;
+        i_link = link;
+        LinkTokenInterface(i_link).approve(i_router, type(uint256).max);
+        APE = ape;
+    }
+
+    receive() external payable {}   
+
+    function updateMsgReceiverAddress(address _receiver) external onlyOwner {
+        receiver = _receiver;
+    }
+   
+    function crossChainBuy(address tokenAddress, uint256 tokenId) external {    
+        ListingDetails memory detail = _listingDetails[tokenAddress][tokenId];
+        IERC20(tokenAddress).transferFrom(msg.sender, address(this), tokenId);
+
+        _listingDetails[tokenAddress][tokenId] = ListingDetails ({
+            listedBy: address(0),
+            price: 0
+        });
+
+        bytes memory data = _encodeCrossChainData(tokenAddress, tokenId, msg.sender);
+        _send(SOURCE_CHAIN_SELECTOR, data);
+
+        emit Buy(block.timestamp, BuyType.CrossChain, msg.sender, tokenAddress, tokenId, detail.price);
+    }
+
+    function _encodeCrossChainData(address tokenAddress, uint256 tokenId, address buyer) internal view returns (bytes memory) {
+        CrossChainBuy memory ccBuy = CrossChainBuy ({
+            buyer: buyer
+        });
+        return abi.encode(tokenAddress, tokenId, _listingDetails[tokenAddress][tokenId], ccBuy);
+    }
+
+    function _decodeListingData(bytes memory data) internal pure returns (
+        address tokenAddress, 
+        uint256 tokenId,
+        ListingDetails memory detail) 
+    {
+        (tokenAddress, tokenId, detail) = abi.decode(data, (address, uint256, ListingDetails));
+    }
+
+    function _send(
+        uint64 destinationChainSelector,
+        bytes memory data
+    ) internal returns (bytes32 messageId) {
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
+            receiver: abi.encode(receiver),
+            data: data,
+            tokenAmounts: new Client.EVMTokenAmount[](0),
+            extraArgs: "",
+            feeToken: i_link
+        });
+
+        messageId = IRouterClient(i_router).ccipSend(
+            destinationChainSelector,
+            message
+        );
+
+        emit MessageSent(messageId, data);
+    }
+
+    function _ccipReceive(
+        Client.Any2EVMMessage memory message
+    ) internal override {
+        (address tokenAddress, uint256 tokenId, ListingDetails memory detail) = _decodeListingData(message.data);
+
+        _listingDetails[tokenAddress][tokenId] = detail;
+            
+        emit MessageReceived(message.messageId, message.data);
+    }
+    
+}
---
+++ src/protocol/AnyApe_Source.sol
@@ -0,0 +1,177 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {Withdraw} from "../utils/Withdraw.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+error Unauthorized();
+
+contract AnyApe_Source is CCIPReceiver, Withdraw {
+
+    uint64 constant SOURCE_CHAIN_SELECTOR = 12532609583862916517; // mumbai
+    uint64 constant DEST_CHAIN_SELECTOR = 14767482510784806043; // fuji
+
+    address public receiver;
+
+    address immutable i_router;
+    address immutable i_link;
+    address immutable public APE;
+
+    enum BuyType {
+        SourceChain,
+        CrossChain
+    }
+
+    struct ListingDetails {
+        address listedBy;
+        uint256 price;
+    }
+    mapping(address => mapping(uint256 => ListingDetails)) private _listingDetails; // tokenAddress => tokenId => ListingDetails
+
+    struct CrossChainBuy {
+        address buyer;
+    }
+
+    event Listing(
+        uint256 indexed timestamp, 
+        address indexed userAddress, 
+        address indexed tokenAddress, 
+        uint256 tokenId,
+        uint256 price
+    );
+
+    event Buy(
+        uint256 indexed timestamp, 
+        BuyType indexed buyType,
+        address indexed userAddress, 
+        address tokenAddress, 
+        uint256 tokenId,
+        uint256 price
+    );
+
+    event Cancel(
+        uint256 indexed timestamp, 
+        address indexed userAddress, 
+        address tokenAddress, 
+        uint256 tokenId
+    );
+
+    event MessageSent(bytes32 messageId, bytes data);
+
+    event MessageReceived(bytes32 messageId, bytes data);
+
+    constructor(address router, address link, address ape) CCIPReceiver(router) {
+        i_router = router;
+        i_link = link;
+        LinkTokenInterface(i_link).approve(i_router, type(uint256).max);
+        APE = ape;
+    }
+
+    receive() external payable {}   
+
+    function updateMsgReceiverAddress(address _receiver) external onlyOwner {
+        receiver = _receiver;
+    }
+
+    function listing(address tokenAddress, uint256 tokenId, uint256 _price) external {
+        IERC721(tokenAddress).safeTransferFrom(msg.sender, address(this), tokenId);
+        _listingDetails[tokenAddress][tokenId] = ListingDetails ({
+            listedBy: msg.sender,
+            price: _price
+        });
+
+        bytes memory data = _encodeListingData(tokenAddress, tokenId);
+        _send(DEST_CHAIN_SELECTOR, data);
+
+        emit Listing(block.timestamp, msg.sender, tokenAddress, tokenId, _price);
+    }
+    
+    function directBuy(address tokenAddress, uint256 tokenId) external {    
+        ListingDetails memory detail = _listingDetails[tokenAddress][tokenId];
+        IERC20(tokenAddress).transferFrom(msg.sender, address(this), detail.price);
+        IERC721(tokenAddress).safeTransferFrom(address(this), msg.sender, tokenId);
+
+        _listingDetails[tokenAddress][tokenId] = ListingDetails ({
+            listedBy: address(0),
+            price: 0
+        });
+
+        bytes memory data = _encodeListingData(tokenAddress, tokenId);
+        _send(DEST_CHAIN_SELECTOR, data);
+
+        emit Buy(block.timestamp, BuyType.SourceChain, msg.sender, tokenAddress, tokenId, detail.price);
+    }
+
+    function cancelListing(address tokenAddress, uint256 tokenId) external {
+        address _listedBy = _listingDetails[tokenAddress][tokenId].listedBy;
+        if (msg.sender != _listedBy) {
+            revert Unauthorized();
+        }
+
+        _listingDetails[tokenAddress][tokenId] = ListingDetails ({
+            listedBy: address(0),
+            price: 0
+        });
+
+        bytes memory data = _encodeListingData(tokenAddress, tokenId);
+        _send(DEST_CHAIN_SELECTOR, data);
+
+        emit Cancel(block.timestamp, msg.sender, tokenAddress, tokenId);
+    }
+
+    function checkListedNftDetails(address tokenAddress, uint256 tokenId) external view returns (ListingDetails memory) {
+        return _listingDetails[tokenAddress][tokenId];
+    }
+
+    function _encodeListingData(address tokenAddress, uint256 tokenId) internal view returns (bytes memory) {
+        return abi.encode(tokenAddress, tokenId, _listingDetails[tokenAddress][tokenId]);
+    }
+
+    function _decodeCrossChainBuy(bytes memory data) internal pure returns (
+        address tokenAddress, 
+        uint256 tokenId,
+        ListingDetails memory detail,
+        CrossChainBuy memory ccBuy) 
+    {
+        (tokenAddress, tokenId, detail, ccBuy) = abi.decode(data, (address, uint256, ListingDetails, CrossChainBuy));
+    }
+
+    function _send(
+        uint64 destinationChainSelector,
+        bytes memory data
+    ) internal returns (bytes32 messageId) {
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
+            receiver: abi.encode(receiver),
+            data: data,
+            tokenAmounts: new Client.EVMTokenAmount[](0),
+            extraArgs: "",
+            feeToken: i_link
+        });
+
+        messageId = IRouterClient(i_router).ccipSend(
+            destinationChainSelector,
+            message
+        );
+
+        emit MessageSent(messageId, data);
+    }
+
+    function _ccipReceive(
+        Client.Any2EVMMessage memory message
+    ) internal override {
+        (address tokenAddress, uint256 tokenId, ListingDetails memory detail, CrossChainBuy memory ccBuy) = _decodeCrossChainBuy(message.data);
+
+        _listingDetails[tokenAddress][tokenId] = detail;
+        IERC721(tokenAddress).safeTransferFrom(address(this), ccBuy.buyer, tokenId);
+            
+        emit Buy(block.timestamp, BuyType.CrossChain, ccBuy.buyer, tokenAddress, tokenId, detail.price);
+        emit MessageReceived(message.messageId, message.data);
+    }
+
+    
+}
