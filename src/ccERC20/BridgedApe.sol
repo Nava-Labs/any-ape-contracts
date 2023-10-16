edb071e05880c65e039d77e731a6aa52ea105f3f
rwin <erwinphanglius10@gmail.com>
2023-10-16 22:25:21 +0700

    init example of BridgedApe

---
+++ src/ccERC20/BridgedApe.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {ERC20, CCIPReceiver, clERC20Proxy_Destination} from "./clERC20Proxy_Destination.sol";
+
+contract BridgedApe is clERC20Proxy_Destination {
+    constructor(
+        string memory name, 
+        string memory symbol, 
+        address router,
+        address tokenAddress,
+        address link
+    ) ERC20(name, symbol) clERC20Proxy_Destination(tokenAddress, router, link) {}
+}
